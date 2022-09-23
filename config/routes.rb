651ed3fb5621fede6e83a0f2335581206cbc6608
Reed MacDonald <reedpmacdonald@gmail.com>
2022-09-23 15:38:02 -0700

    Changed routes for bayern players

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   #get 'home/index'
   root 'home#index'
   get 'home/about'
+  get 'bayern_players/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
